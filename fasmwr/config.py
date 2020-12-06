386ecb736d32f409261ac910a3a4e99972bb0c3d
Tim P <timothypoehlman@gmail.com>
2020-12-05 17:10:28 -0800

    added database capabilities

---
+++ fasmwr/config.py
@@ -0,0 +1,28 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'YOUR SECRET KEY HERE'
+    SQLALCHEMY_DATABASE_URI = os.environ['FASMWR_DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
