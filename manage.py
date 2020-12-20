ca28cf78f59ca734fa2a284d485e7a3461c3044b
Tim P <timothypoehlman@gmail.com>
2020-12-19 17:23:25 -0800

    removed manage.py temp[

--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
 # manage.py is used to migrate the database when making modifications to models.py.
 
-import os
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+# import os
+# from flask_script import Manager
+# from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
-
-
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
+# from app import app, db
 
 
-if __name__ == '__main__':
-    manager.run()
+# app.config.from_object(os.environ['APP_SETTINGS'])
+
+# migrate = Migrate(app, db)
+# manager = Manager(app)
+
+# manager.add_command('db', MigrateCommand)
+
+
+# if __name__ == '__main__':
+#     manager.run()
